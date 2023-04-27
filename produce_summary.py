39adf3d844aa1a5742dd749745f768bbef52d399
tjnalls <thea.joanna.nalls@gmail.com>
2023-04-26 23:20:18 -0700

    comments added

--- produce_summary.py
+++ produce_summary.py
@@ -6,18 +6,26 @@ def melon_count():
     
     i = 0
     while i < len(filenames):
+    '''checks how many delivery days'''
+
         print(f"Day {i + 1}")
+        '''displays referenced day log'''
         the_file = open(filenames[i])
         for line in the_file:
             line = line.rstrip()
             words = line.split('|')
+        '''separates out each line of the txt doc file into list items'''
 
             melon = words[0]
             count = words[1]
             amount = words[2]
+        '''assigns variable names for each item of the list'''
 
             print(f"Delivered {count} {melon}s for total of ${amount}")
+        '''displays desired string of each log'''
         the_file.close()
+        '''closes out the current doc file'''
         i = i + 1
+        '''cycles to next indexed doc in filenames'''
     
 melon_count()
